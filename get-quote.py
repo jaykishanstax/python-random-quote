05755dde9762dc943c70413cf9fe172943506d94
jaykishan.hirpara <jaykishan.hirpara@motadata.com>
2021-05-02 13:18:27 +0530

    File write function added

--- get-quote.py
+++ get-quote.py
@@ -9,7 +9,32 @@ def test():
 
   last = len(quotes)-1
   rnd = random.randint(0, last)
-  print(quotes[rnd])
+  rnd2 = random.randint(0, last)
+  print(quotes[rnd].strip())
+  print(quotes[rnd2].strip())
+
+def append_multiple_lines(file_name, lines_to_append):
+    # Open the file in append & read mode ('a+')
+    with open(file_name, "a+") as file_object:
+        appendEOL = False
+        # Move read cursor to the start of file.
+        file_object.seek(0)
+        # Check if file is not empty
+        data = file_object.read(100)
+        if len(data) > 0:
+            appendEOL = True
+        # Iterate over each string in the list
+        for line in lines_to_append:
+            # If file is not empty then append '\n' before first line for
+            # other lines always append '\n' before appending line
+            if appendEOL == True:
+                file_object.write("\n")
+            else:
+                appendEOL = True
+            # Append element at the end of file
+            file_object.write(line)
 
 if __name__== "__main__":
-  test()
+
+    append_multiple_lines("quotes.txt", ['New Line added by JK1', 'New Line by JK2', 'New Data on last line'])
+    test()
